85c1edc2a82f2dbdc4d6c20b2d2143cbdac64d91
AmirovNodirbek <nodiramerov5@gmail.com>
2025-02-15 07:45:47 +0500

    O'zgarish haqida qisqacha izoh

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+function showNewCard() {
+    let card = document.getElementById("newCard");
+    if (card.style.display === "none" || card.style.display === "") {
+        card.style.display = "block";
+    } else {
+        card.style.display = "none";
+    }
+}
 
+
+function handleSelectChange(select) {
+    console.log("Selected value:", select.value);
+}
